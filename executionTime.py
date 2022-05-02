c5c8d6aead6357fbfabf85a8ea94a353ad26cad1
Ivan Camilo <ibarragan@unal.edu.co>
2022-05-02 02:45:37 -0500

    it was changed the module used and the names of the functions

--- executionTime.py
+++ executionTime.py
@@ -1,27 +1,23 @@
-from datetime import timedelta, datetime
+import time
 
 
-def howlong(func, *args, **kwargs):
-    """this function calculate execution time of a function"""
-    star = datetime.now()
-    originReturn = func(*args, **kwargs)
-    end = datetime.now()
-    delta = (end - star)/timedelta(milliseconds=1)
-    print('Execution time is: ', round(delta, 4))
-    return originReturn
-
-
-def decorator(func):
+def exTime(func):
     """use how @decorator for it calculate execution time of a function"""
     def aux(*args, **kwargs):
-        star = datetime.now()
+        star = time.time()
         originReturn = func(*args, **kwargs)
-        end = datetime.now()
-        delta = (end - star)/timedelta(milliseconds=1)
+        end = time.time()
+        delta = (end - star)*1000 #in milliseconds
         print('Execution time is: ', round(delta, 4))
         return originReturn
     return aux
 
 
-#probar directamente con time()
-#cambiar ubicación y subir a git
+def duration(func, *args, **kwargs):
+    """this function calculate execution time of a function 'func'"""
+    star = time.time()
+    originReturn = func(*args, **kwargs)
+    end = time.time()
+    delta = (end - star)*1000 #in milliseconds
+    print('Execution time is: ', round(delta, 4))
+    return originReturn
