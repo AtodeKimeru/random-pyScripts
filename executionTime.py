9283c609af41e48bc21af11e530a5202cce37e35
Ivan Camilo <ibarragan@unal.edu.co>
2022-05-04 03:29:23 -0500

    corrections in executiontime.py

--- executionTime.py
+++ executionTime.py
@@ -3,14 +3,14 @@ import time
 
 def exTime(func):
     """use how @decorator for it calculate execution time of a function"""
-    def aux(*args, **kwargs):
+    def wrap(*args, **kwargs):
         star = time.time()
         originReturn = func(*args, **kwargs)
         end = time.time()
         delta = (end - star)*1000 #in milliseconds
         print('Execution time is: ', round(delta, 4))
         return originReturn
-    return aux
+    return wrap
 
 
 def duration(func, *args, **kwargs):
